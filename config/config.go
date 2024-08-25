ad4c94457e10d51dbbfd0385b3d09ec28d51e513
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-25 23:45:33 +0330

    ✨feature: new conf added

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type (
 	Config struct {
 		APP  `yaml:"app"`
@@ -14,3 +20,18 @@ type (
 		Port string `env-required:"true" yaml:"port"`
 	}
 )
+
+func NewConfig() (*Config, error) {
+	conf := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yaml", conf)
+	if err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	err = cleanenv.ReadEnv(conf)
+	if err != nil {
+		return nil, fmt.Errorf("env error: %w", err)
+	}
+	return conf, nil
+}
