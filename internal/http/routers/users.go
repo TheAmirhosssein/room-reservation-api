bc1698ae5e9ab8029c74cff56316625cd7d15b05
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-28 22:50:46 +0000

    ✨feature: auth  router added

---
+++ internal/http/routers/users.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"github.com/TheAmirhosssein/room-reservation-api/internal/http/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRouters(server *gin.Engine, prefix string) {
+	userRouter := server.Group(prefix)
+	userRouter.POST("authenticate", handlers.Authenticate)
+}
