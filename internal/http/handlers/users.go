123fdd4cd11fcec38fd867e08035c3661f289527
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-28 22:49:46 +0000

    ✨feature: get user by mobile number added into project

--- internal/http/handlers/users.go
+++ internal/http/handlers/users.go
@@ -1,11 +1,25 @@
 package handlers
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/TheAmirhosssein/room-reservation-api/internal/app/database"
+	"github.com/TheAmirhosssein/room-reservation-api/internal/entity"
+	"github.com/TheAmirhosssein/room-reservation-api/internal/repository"
 	"github.com/gin-gonic/gin"
 )
 
-func register(context *gin.Context) {
-	fmt.Println("something")
+func Authenticate(context *gin.Context) {
+	user := entity.User{}
+	err := context.BindJSON(&user)
+	if err != nil {
+		context.JSONP(http.StatusBadRequest, err.Error())
+		return
+	}
+	userRepo := repository.NewUsersRepository(database.DB)
+	userRepo.ByMobileNumber(user.MobileNumber, &user)
+	if user.ID == 0 {
+		userRepo.Save(&user)
+	}
+	context.JSONP(http.StatusOK, user)
 }
