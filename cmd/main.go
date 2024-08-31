0d045ae6e6a8f8f8c99ec801fce3a462fa3b36c7
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-31 23:35:33 +0000

    ✨feature: redis added into project

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/TheAmirhosssein/room-reservation-api/config"
 	"github.com/TheAmirhosssein/room-reservation-api/internal/infrastructure/database"
+	"github.com/TheAmirhosssein/room-reservation-api/internal/infrastructure/redis"
 	"github.com/TheAmirhosssein/room-reservation-api/internal/infrastructure/server"
 )
 
@@ -13,6 +14,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Config error: %s", err)
 	}
+	redis.NewConnection(conf)
 	database.StartDB(conf)
 	server.Run(conf)
 }
