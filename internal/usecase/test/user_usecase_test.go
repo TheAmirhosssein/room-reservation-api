5f0b50c839b42b1737649b057d4819769b94e959
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-04 22:11:24 +0000

    🧪test: more test added into test use case

--- internal/usecase/test/user_usecase_test.go
+++ internal/usecase/test/user_usecase_test.go
@@ -19,7 +19,16 @@ func TestUserUseCase_GetUserOrCreate(t *testing.T) {
 	repo := repository.NewUserRepository(db)
 	userUseCase := usecase.NewUserUseCase(repo)
 	mobileNumber := "090012305412"
+	var count int64
+	db.Model(&entity.User{}).Count(&count)
 	user, err := userUseCase.GetUserOrCreate(mobileNumber)
 	assert.NoError(t, err)
 	assert.Equal(t, user.MobileNumber, mobileNumber)
+	var countAfter int64
+	db.Model(&entity.User{}).Count(&countAfter)
+	count += 1
+	assert.Equal(t, count, countAfter)
+	_, err = userUseCase.GetUserOrCreate(mobileNumber)
+	assert.NoError(t, err)
+	assert.Equal(t, count, countAfter)
 }
