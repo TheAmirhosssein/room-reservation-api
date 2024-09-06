36d3e3af46e5c8cb8b2509608798256c87945a44
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-06 15:56:41 +0000

    ✨feature: jwt generate token func crated

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"log"
+	"time"
+
+	"github.com/TheAmirhosssein/room-reservation-api/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateAccessToken(userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userId,
+		"exp":     time.Now().Add(time.Hour * (24 * 365)).Unix(),
+	})
+
+	conf, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	secretKey := conf.APP.SecretKey
+	return token.SignedString([]byte(secretKey))
+}
