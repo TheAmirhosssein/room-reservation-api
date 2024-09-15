3cc4d6573375ed0e59e484b5e965eefa333a305c
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-15 22:00:37 +0000

    ✨feature: check role exist by name added

--- internal/repository/role_repository.go
+++ internal/repository/role_repository.go
@@ -1,12 +1,15 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/TheAmirhosssein/room-reservation-api/internal/entity"
 	"gorm.io/gorm"
 )
 
 type RoleRepository interface {
 	Save(*entity.Role) error
+	ExitsByName(name string) bool
 }
 
 type roleRepository struct {
@@ -19,4 +22,10 @@ func NewRoleRepository(db *gorm.DB) RoleRepository {
 
 func (roleRepo roleRepository) Save(role *entity.Role) error {
 	return roleRepo.db.Save(role).Error
+}
+
+func (roleRepo roleRepository) ExitsByName(name string) bool {
+	var role entity.Role
+	err := roleRepo.db.First(&role, "name = ?", name).Error
+	return !errors.Is(err, gorm.ErrRecordNotFound)
 }
--- internal/repository/test/role_repository_test.go
+++ internal/repository/test/role_repository_test.go
@@ -29,3 +29,21 @@ func TestRoleRepository_Test(t *testing.T) {
 	db.Model(&entity.Role{}).Count(&countAfterSave)
 	assert.Equal(t, countAfterSave, int64(1))
 }
+
+func TestRoleRepository_ExitsByName(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+	err = database.Migrate(db)
+	assert.NoError(t, err)
+	repo := repository.NewRoleRepository(db)
+
+	exist := repo.ExitsByName("something")
+	assert.False(t, exist)
+
+	role := entity.NewRole("something")
+	err = repo.Save(&role)
+	assert.NoError(t, err)
+
+	exist = repo.ExitsByName("something")
+	assert.True(t, exist)
+}
