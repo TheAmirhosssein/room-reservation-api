ae0130931c5862ce899f8e1e2f441b716d82209c
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-23 16:42:41 +0000

    ✨feature: cors added into project

--- internal/infrastructure/server/app.go
+++ internal/infrastructure/server/app.go
@@ -6,6 +6,7 @@ import (
 	"github.com/TheAmirhosssein/room-reservation-api/config"
 	"github.com/TheAmirhosssein/room-reservation-api/docs"
 	"github.com/TheAmirhosssein/room-reservation-api/internal/http/routers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -14,7 +15,13 @@ import (
 func Run(conf *config.Config) {
 	server := gin.Default()
 	routers.UserRouters(server, "/api/v1/user")
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	server.Use(cors.New(config))
+
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	server.Run(fmt.Sprintf("%v:%v", conf.HTTP.Host, conf.HTTP.Port))
 }
