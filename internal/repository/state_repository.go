fe780913b306e2073a908ad962aaf78c5b23a025
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-24 15:03:21 +0000

    ✨feature: states list added

--- internal/repository/state_repository.go
+++ internal/repository/state_repository.go
@@ -9,6 +9,7 @@ import (
 
 type StateRepository interface {
 	Save(context.Context, *entity.State) *gorm.DB
+	StateList(context.Context, string) ([]entity.State, *gorm.DB)
 }
 
 type stateRepository struct {
@@ -21,4 +22,12 @@ func NewStateRepository(db *gorm.DB) StateRepository {
 
 func (stateRepo stateRepository) Save(ctx context.Context, state *entity.State) *gorm.DB {
 	return stateRepo.db.WithContext(ctx).Save(state)
+}
+
+func (stateRepo stateRepository) StateList(ctx context.Context, title string) ([]entity.State, *gorm.DB) {
+	var states []entity.State
+	query := stateRepo.db.Model(&entity.State{}).
+		Where("title LIKE ?", "%"+title+"%").
+		Find(&states)
+	return states, query
 }
--- internal/repository/test/state_repository_test.go
+++ internal/repository/test/state_repository_test.go
@@ -32,3 +32,35 @@ func TestStateRepository_Test(t *testing.T) {
 
 	assert.Equal(t, state.Title, savedState.Title)
 }
+
+func TestStateRepository_StateList(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something else")
+	repo.Save(ctx, &state)
+
+	otherState := entity.NewState("something")
+	repo.Save(ctx, &otherState)
+
+	var count int64
+	db.Model(&entity.State{}).Count(&count)
+
+	states, query := repo.StateList(ctx, "")
+	assert.NoError(t, query.Error)
+	assert.Equal(t, int(count), len(states))
+
+	states, query = repo.StateList(ctx, "else")
+	assert.NoError(t, query.Error)
+	assert.Equal(t, len(states), 1)
+
+	states, query = repo.StateList(ctx, "something")
+	assert.NoError(t, query.Error)
+	assert.Equal(t, len(states), 2)
+}
