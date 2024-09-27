5969c23c9e753f0e46b31cf2aabb06665da840b0
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-27 18:08:51 +0000

    ✨feature: some methods added into city repository

--- internal/repository/city_repository.go
+++ internal/repository/city_repository.go
@@ -11,6 +11,10 @@ type CityRepository interface {
 	Save(context.Context, *entity.City) *gorm.DB
 	List(context.Context, string, int) ([]entity.City, *gorm.DB)
 	Paginate(int, int, *gorm.DB) ([]entity.City, error)
+	Count(context.Context) (int, error)
+	ById(context.Context, uint, *entity.City) *gorm.DB
+	Update(context.Context, *entity.City, map[string]any) error
+	Delete(context.Context, *entity.City) *gorm.DB
 }
 
 type cityRepository struct {
@@ -38,4 +42,22 @@ func (repo cityRepository) Paginate(limit, offset int, query *gorm.DB) ([]entity
 	var cities []entity.City
 	err := query.Limit(limit).Offset(offset).Find(&cities).Error
 	return cities, err
+}
+
+func (repo cityRepository) Count(ctx context.Context) (int, error) {
+	var count int64
+	err := repo.db.Model(&entity.City{}).Count(&count).Error
+	return int(count), err
+}
+
+func (repo cityRepository) ById(ctx context.Context, id uint, city *entity.City) *gorm.DB {
+	return repo.db.WithContext(ctx).Preload("State").First(&city, "ID = ?", id)
+}
+
+func (repo cityRepository) Update(ctx context.Context, city *entity.City, newInfo map[string]any) error {
+	return repo.db.WithContext(ctx).Model(&city).Updates(newInfo).Error
+}
+
+func (repo cityRepository) Delete(ctx context.Context, city *entity.City) *gorm.DB {
+	return repo.db.WithContext(ctx).Delete(city)
 }
