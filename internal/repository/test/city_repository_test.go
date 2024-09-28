bf02e3a70ec9379fa289cec0932765f674fe5d06
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-28 19:09:48 +0000

    🧪test: city repository test added into project

--- internal/repository/test/city_repository_test.go
+++ internal/repository/test/city_repository_test.go
@@ -110,3 +110,97 @@ func TestCityRepository_Paginate(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, len(cities), 1)
 }
+
+func TestCityRepository_Count(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	stateRepo.Save(ctx, &state)
+
+	city := entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+	count, err := repo.Count(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, count, 1)
+}
+
+func TestCityRepository_ById(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	stateRepo.Save(ctx, &state)
+
+	city := entity.City{}
+	repo.ById(ctx, 1, &city)
+	assert.Equal(t, uint(0), city.ID)
+
+	city = entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+	assert.Equal(t, uint(1), city.ID)
+}
+
+func TestCityRepository_Update(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	stateRepo.Save(ctx, &state)
+
+	city := entity.NewCity("something", state)
+	query := repo.Save(ctx, &city)
+	assert.NoError(t, query.Error)
+
+	err = repo.Update(ctx, &city, map[string]any{"title": "something else"})
+	assert.NoError(t, err)
+	assert.Equal(t, city.Title, "something else")
+}
+
+func TestCityRepository_Delete(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	stateRepo.Save(ctx, &state)
+
+	city := entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+	var count int64
+	db.Model(&entity.City{}).Count(&count)
+
+	repo.Delete(ctx, &city)
+	var countAfterDelete int64
+	db.Model(&entity.City{}).Count(&countAfterDelete)
+
+	assert.Equal(t, countAfterDelete, count-1)
+}
