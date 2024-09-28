cd39ed4977febd20cc9af1ab413528cd9abc64c9
amirhossein <amirhosseinmobini9@gmail.com>
2024-09-28 20:40:23 +0000

    🧪test: city usecase test added into project

--- internal/usecase/test/city_usecase_test.go
+++ internal/usecase/test/city_usecase_test.go
@@ -42,3 +42,142 @@ func TestCityUseCase_Create(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, countAfterCreate, citiesCount+1)
 }
+
+func TestCityUseCase_List(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	err = stateRepo.Save(ctx, &state).Error
+	assert.NoError(t, err)
+
+	useCase := usecase.NewCityUseCase(repo)
+
+	city := entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+	newCity := entity.NewCity("something else", state)
+	repo.Save(ctx, &newCity)
+
+	var count int64
+	db.Model(&entity.City{}).Count(&count)
+
+	cities, err := useCase.CityList(ctx, 1, 1, 1, "")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(cities))
+
+	cities, err = useCase.CityList(ctx, 1, 10, 1, "something else")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(cities))
+}
+
+func TestCityUseCase_DoesCityExist(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	cityUseCase := usecase.NewCityUseCase(repo)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	err = stateRepo.Save(ctx, &state).Error
+	assert.NoError(t, err)
+
+	result := cityUseCase.DoesCityExist(ctx, 1)
+	assert.False(t, result)
+
+	city := entity.NewCity("something", state)
+	err = repo.Save(ctx, &city).Error
+	assert.NoError(t, err)
+	result = cityUseCase.DoesCityExist(ctx, 1)
+	assert.True(t, result)
+}
+
+func TestCityRepository_GetCityById(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	err = stateRepo.Save(ctx, &state).Error
+	assert.NoError(t, err)
+
+	cityUseCase := usecase.NewCityUseCase(repo)
+	_, err = cityUseCase.ById(ctx, 1)
+	assert.Error(t, err)
+
+	city := entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+
+	_, err = cityUseCase.ById(ctx, 1)
+	assert.NoError(t, err)
+}
+
+func TestCityUseCase_Update(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	useCase := usecase.NewCityUseCase(repo)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	err = stateRepo.Save(ctx, &state).Error
+	assert.NoError(t, err)
+
+	city := entity.NewCity("something", state)
+	err = repo.Save(ctx, &city).Error
+
+	assert.NoError(t, err)
+	err = useCase.Update(ctx, city.ID, map[string]any{"Title": "something else"})
+	assert.NoError(t, err)
+	repo.ById(ctx, city.ID, &city)
+	assert.Equal(t, city.Title, "something else")
+}
+
+func TestCityUseCase_DeleteById(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+	database.Migrate(db)
+	repo := repository.NewCityRepository(db)
+	useCase := usecase.NewCityUseCase(repo)
+	stateRepo := repository.NewStateRepository(db)
+
+	state := entity.NewState("something")
+	err = stateRepo.Save(ctx, &state).Error
+	assert.NoError(t, err)
+
+	city := entity.NewCity("something", state)
+	repo.Save(ctx, &city)
+	var count int64
+	db.Model(&entity.City{}).Count(&count)
+
+	err = useCase.DeleteById(ctx, city.ID)
+	assert.NoError(t, err)
+	var countAfterDelete int64
+	db.Model(&entity.City{}).Count(&countAfterDelete)
+
+	assert.Equal(t, countAfterDelete, count-1)
+}
